e3d33ac68d816ceda0266e4be0586e148d971bc0
unknown <daza.ronnie.09212002@gmail.com>
2021-11-15 14:51:44 +0800

    Ask the user for the first number Commit

---
+++ Program 2 - Find the Lowest Number of the Three.py
@@ -0,0 +1,9 @@
+# Program 2 - Find the Lowest Number of the Three
+# Find the lowest number.
+# Create a program that asks 3 numbers.
+# Find the lowest number using only if-else statement.
+# Display the lowest number.
+
+# Steps
+# 1. Ask the user for the first number.
+firstNumber = float(input("Enter the first number: "))
