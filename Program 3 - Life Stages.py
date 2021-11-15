9213cedd013084eba61b1b3956190f485998bf37
unknown <daza.ronnie.09212002@gmail.com>
2021-11-15 15:01:34 +0800

    Test if the user is a teenager Commit

--- Program 3 - Life Stages.py
+++ Program 3 - Life Stages.py
@@ -13,3 +13,7 @@ age = int(input("Enter your age: "))
 # 2. Test if the user is a kid.
 if age >-1 and age <=12:
     print("You're a kid.") # True Block
+else:
+    # 3. Test if the user is a teenager.
+    if age >=13 and age <=17:
+        print("You're a teenager.") # True Block
