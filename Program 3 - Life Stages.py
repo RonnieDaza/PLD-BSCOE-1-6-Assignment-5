ebe3f16fb89ab94299f39b0eb59b4291c4d74579
unknown <daza.ronnie.09212002@gmail.com>
2021-11-15 15:00:17 +0800

    Ask for age, convert, and store Commit

---
+++ Program 3 - Life Stages.py
@@ -0,0 +1,12 @@
+# Program 3 - Life Stages
+# Create a program that asks for an age of a person.
+# Display the life stage of the person.
+# Rules:
+# 0 - 12: Kid
+# 13 - 17: Teen
+# 18: Debut
+# 19 and above: Adult
+
+# Steps
+# 1. Ask for age, convert, and store.
+age = int(input("Enter your age: "))
